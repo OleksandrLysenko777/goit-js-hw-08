bd88c34b1e9840aff3426d81262154d29cad80de
OleksandrLysenko777 <114435880+OleksandrLysenko777@users.noreply.github.com>
2023-03-13 22:45:14 +0100

    mmm

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,26 +5,32 @@ form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
 let formData = {};
+loadForm();
 
 function onFormData(e) {
+  formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
   formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   e.preventDefault();
-  e.currentTarget.reset();
+  e.target.reset();
+  console.log(formData);
   localStorage.removeItem('feedback-form-state');
-  formData = {};
 }
 
-(function dataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
+function loadForm() {
+  try {
+    let formLoad = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (!formLoad) {
+      return;
+    }
+
+    formData = formLoad;
+    form.email.value = formData.email || '';
+    form.message.value = formData.message || '';
+  } catch (error) {
+    console.error('Error.message ', error.message);
   }
-})();
+}
